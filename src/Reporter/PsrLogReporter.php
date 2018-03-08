39afb362114267bc4ecc9310108e1b5d9bff296e
Witold Wasiczko <witold.wasiczko@rst.com.pl>
2018-03-08 17:02:46 +0100

    Fix reporter

--- src/Reporter/PsrLogReporter.php
+++ src/Reporter/PsrLogReporter.php
@@ -30,13 +30,17 @@ final class PsrLogReporter implements ReporterInterface
 
     public function onBeforeRun(CheckInterface $check, $checkAlias = null)
     {
-        $this->checks[$check] = time();
+        $this->checks[$check] = [
+            'time' => time(),
+            'check' => $check,
+        ];
     }
 
     public function onAfterRun(CheckInterface $check, ResultInterface $result, $checkAlias = null)
     {
         if (isset($this->checks[$check])) {
-            $this->checks[$check] = time() - $this->checks[$check];
+            $this->checks[$check]['time'] = time() - $this->checks[$check]['time'];
+            $this->checks[$check]['result'] = $result;
             return;
         }
         throw new Exception('Check not started');
@@ -45,12 +49,16 @@ final class PsrLogReporter implements ReporterInterface
     public function onFinish(Collection $results)
     {
         $checks = [];
-        foreach ($this->checks as $check => $time) {
+        foreach ($this->checks as $check => $data) {
             /* @var $check CheckInterface */
             $checks[] = [
                 'class' => get_class($check),
                 'label' => $check->getLabel(),
-                'time' => $time,
+                'result' => [
+                    'message' => $data['result']->getMessage(),
+                    'data' => $data['result']->getData(),
+                    'time' => $data['time'],
+                ],
             ];
         }
 
